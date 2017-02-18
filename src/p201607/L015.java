5f0556ae0b3f2424ab62df744f6c37320d89d6bb
zhanyd <zhanyd@sina.com>
2017-02-18 21:24:12 +0800

    add L015

---
+++ src/p201607/L015.java
@@ -0,0 +1,12 @@
+package p201607;
+
+/**
+ * Created by Administrator on 2017/2/18 0018.
+ */
+public class L015 {
+
+    public static void main(String[] args){
+
+        System.out.println("asd");
+    }
+}
