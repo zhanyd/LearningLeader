5ef5e39d070fc29ab322eb30337857aae7720777
zhanyd <zhanyd@sina.com>
2018-11-07 16:29:48 +0800

    Add

--- src/P201611/transferFile/TraditionalClient.java
+++ src/P201611/transferFile/TraditionalClient.java
@@ -44,7 +44,7 @@ public class TraditionalClient {
 		
 	    output = new DataOutputStream(socket.getOutputStream());
 	    long start = System.currentTimeMillis();	    
-	    byte[] b = new byte[4096];
+	    byte[] b = new byte[100];
 	    long read = 0, total = 0;
 	    while((read = inputStream.read(b))>=0) {
 		total = total + read;
